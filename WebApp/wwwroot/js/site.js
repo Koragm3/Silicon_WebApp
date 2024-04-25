2378f5dfc5a49e300a22c633b9c6fa3e635c14f0
Koragm3 <hasanserro@gmail.com>
2024-04-25 12:25:29 +0200

    form validation added

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -17,3 +17,38 @@ function handleProfileImageUpload() {
     }
     catch { }
 }
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('form');
+form.addEventListener('submit', function (event) {
+        var isValid = true;
+
+
+var inputs = form.querySelectorAll('input');
+inputs.forEach(function (input) {
+            if (input.value.trim() === '') {
+    isValid = false;
+var errorMessage = input.nextElementSibling;
+errorMessage.innerText = 'This field is required.';
+            }
+        });
+
+if (!isValid) {
+    event.preventDefault();
+        }
+    });
+
+
+form.addEventListener('focus', function (event) {
+        if (event.target.tagName === 'INPUT') {
+            var errorMessage = event.target.nextElementSibling;
+errorMessage.innerText = '';
+        }
+    }, true); 
+});
+  
+
